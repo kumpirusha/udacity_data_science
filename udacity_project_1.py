0738d1da388b460c3cb44dda984cbdeb608922f3
kumpirusha <jaka.vrhovnik.1001@gmail.com>
2021-06-02 22:22:59 +0200

    Work at home:  - further data cleaning and preparing for LR

--- udacity_project_1.py
+++ udacity_project_1.py
@@ -35,10 +35,6 @@ listings_full = pd.merge(left=calendar_csv, right=listings_subset, how='left', l
 # check missing values
 miss_full = listings_full.isnull().mean() * 100
 
-# reformat price columns into a float dtype
-for col in ['price_x', 'weekly_price', 'cleaning_fee', 'extra_people']:
-    listings_full[col] = listings_full[col].str.replace(',', '').str.replace('$', '').astype(float)
-
 # remove URL columns which are redundant
 url_cols = listings_full.filter(like='url')
 listings_full = listings_full.loc[:, ~listings_full.columns.isin(url_cols.columns)]
@@ -49,7 +45,8 @@ listings_full.rename(columns={'price_x': 'price'}, errors='raise', inplace=True)
 # replace values with meaningful descriptors
 cols_to_replace = {
     'host_is_superhost': {'t': 'superhost', 'f': 'regular'},
-    'available': {'t': 'free', 'f': 'booked'}
+    'available': {'t': 'free', 'f': 'booked'},
+
 }
 
 for k, v in cols_to_replace.items():
@@ -72,6 +69,27 @@ for i in listings_full.columns:
 
 listings_full = listings_full.loc[:, ~listings_full.columns.isin(cols_len_one)]
 
+# reformat price columns into a float dtype
+for col in ['price_x', 'weekly_price', 'cleaning_fee', 'extra_people']:
+    listings_full[col] = listings_full[col].str.replace(',', '').str.replace('$', '').astype(float)
+
+# reformat 'host_response_rate'
+listings_full.host_response_rate = listings_full.host_response_rate.str.replace('%', '').astype(float)/100
+
+
+
+listings_full.info()
+
+for i in listings_full:
+    print(i, '---', listings_full[i].unique(), '\n','*' * 50)
+
+# further remove redundant columns
+cols_to_drop = ['name', 'summary', 'space', 'description', 'neighborhood_overview', 'notes', 'transit',
+                'host_name', 'host_location', 'host_about', 'street', 'neighborhood_overview', 'zipcode',
+                'smart_location', 'latitude', 'longitude', 'price_y', 'calendar_updated', 'neighbourhood',
+                'host_acceptance_rate', 'state', 'city', 'is_location_exact']
+
+listings_full = listings_full.loc[:, ~listings_full.columns.isin(cols_to_drop)]
 
 ##############################################################################################################
 ##############################################################################################################
@@ -85,7 +103,6 @@ value_listings_db = listings_full.iloc[:, value_cols]
 value_listings_db.room_type = value_listings_db.room_type.astype('category')
 value_listings_db.review_scores_value = value_listings_db.review_scores_value.astype('category')
 
-
 # Correlation
 num_cols = []
 
@@ -95,14 +112,25 @@ for i in value_listings_db:
         num_cols.append(i)
 
 value_cor_db = value_listings_db.iloc[:, value_listings_db.columns.isin(num_cols)]
-value_cor_miss = value_cor_db.isnull().mean()*100
+value_cor_miss = value_cor_db.isnull().mean() * 100
+
+# handpick relevant variables
+value_cor_hp = value_listings_db.iloc[:,
+               [2, 3, 9, 10, 12, 14, 17, 18, 19, 20, 30, 31, 32, 33, 34, 35, 36, 40, 41, 49, 52, 53, 54,
+                55, 56, 57, 58, 59, 60, 64]]
+
+pd.get_dummies(value_cor_hp, columns=['available', 'host_response_time','host_is_superhost',
+                                      'host_has_profile_pic', 'host_identity_verified'])
+
+for i in value_cor_hp:
+    print(i, '---', value_cor_hp[i].unique(), '\n','*' * 50)
+
 
 # check for correlation between variables
-value_cor = value_cor_db.corr()
-matrix = np.triu(value_cor_db.corr())
-sns.heatmap(value_cor[value_cor > 0.6], annot=True, fmt='.1g', vmin=-1, vmax=1, center=0, cmap='coolwarm',
+value_cor = value_cor_hp.corr()
+matrix = np.triu(value_cor_hp.corr())
+sns.heatmap(value_cor, annot=True, fmt='.1g', vmin=-1, vmax=1, center=0, cmap='coolwarm',
             mask=matrix)
-
 
 # Plot variables to check for differences and predictive value
 # property type
@@ -117,7 +145,6 @@ value_listings_db.pivot_table(index=['date'], values='price', columns='room_type
 value_listings_db.pivot_table(values='listing_id', index='review_scores_rating',
                               aggfunc=pd.Series.nunique).plot(kind='bar')
 
-
 # people it accomodates
 value_listings_db.pivot_table(values='listing_id', index='accommodates', aggfunc=pd.Series.nunique).plot(
     kind='bar')
@@ -130,6 +157,8 @@ value_listings_db.pivot_table(index=['year', 'month'],
                               columns=['cancellation_policy'],
                               aggfunc=[np.mean]).plot(grid=True)
 
+# apply a multiple regression model
+model = LinearRegression
 
 # # pivot the table to get some basic insights into the data
 # fig, ax = plt.subplots()
@@ -147,7 +176,6 @@ value_listings_db.pivot_table(index=['year', 'month'],
 
 # Superhost
 listings_full.pivot_table(index=['year', 'month'], values='review_scores_accuracy', aggfunc=np.count)
-
 
 listings_full.pivot_table(
     index=['host_is_superhost', 'available'], values='price', aggfunc=[np.mean, len]
